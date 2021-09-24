ae5a7ff7735106e2ee68ca1c75907faca8a98d27
Tim Roeder <timothyjroeder@gmail.com>
2021-09-24 02:13:28 -0600

    Refactored ex2.rb, syntax and format changes

--- section1/exercises/ex2.rb
+++ section1/exercises/ex2.rb
@@ -1,9 +1,11 @@
+# Learn Ruby the Hard Way exercise #2.
+
 # A comment, this is so you can read your program later.
 # Anything after the # is ignored by ruby.
 
-puts "I could have code like this." # and the comment after is ignored
+puts 'I could have code like this.' # The comment after is ignored.
 
 # You can also use a comment to "disable" or comment out a piece of code:
 # puts "This won't run."
 
-puts "This will run."
+puts 'This will run.'
