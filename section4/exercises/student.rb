0eed7dd69c74b8aeb883aefceaabb715bc913dc7
Tim Roeder <timothyjroeder@gmail.com>
2021-09-24 17:10:04 -0600

    Refactored student.rb, added annotations

--- section4/exercises/student.rb
+++ section4/exercises/student.rb
@@ -1,16 +1,25 @@
+# Create new Student class. Accepts no initial variables.
 class Student
+  # Create read write attribute methods.
   attr_accessor :first_name, :last_name, :primary_phone_number
 
+  # Method accepts string and puts string interpolation with first_name
+  #   attribute.
   def introduction(target)
     puts "Hi #{target}, I'm #{first_name}!"
   end
-
+  
+  # Method returns the integer 7.
   def favorite_number
     7
   end
 end
 
+# Create new class instance frank.
 frank = Student.new
-frank.first_name = "Frank"
+# Set first_name variable to string "Frank"
+frank.first_name = 'Frank'
+# Test introduction method.
 frank.introduction('Katrina')
+# Test favorite_number method.
 puts "Frank's favorite number is #{frank.favorite_number}."
