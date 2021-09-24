d13b3685e4680a993cfb50f5b5a0d2a87c9752d0
Tim Roeder <timothyjroeder@gmail.com>
2021-09-24 16:51:32 -0600

    Refactored my_car.rb, added annotations

--- section4/exercises/my_car.rb
+++ section4/exercises/my_car.rb
@@ -1,8 +1,13 @@
 # Launch School MyCar class exercise
+
+# Declare MyCar class
 class MyCar
-    attr_accessor :color
+    # Declare read/write enabled attribute color.
+    attr_accessor :color, :model
+    # Declare read only enabled attribute year.
     attr_reader :year
 
+    # Initalize attributes and set initial value for current_speed attribute.
     def initialize(y, c, m)
         @year = y
         @color = c
@@ -10,36 +15,44 @@ class MyCar
         @current_speed = 0
     end
 
-    # inc_speed will increase the current speed by the set_speed setpoint, and print the new current speed
+    # Method inc_speed will increase the current speed by the set_speed
+    #   setpoint, and print the new current speed.
     def inc_speed(set_speed)
         @current_speed += set_speed.abs
         puts "Increase the current speed to #{@current_speed} MPH"
     end
 
-    # dec_speed will decrease the current speed by the set_speed setpoint, and print the new current speed
+    # Method dec_speed will decrease the current speed by the set_speed
+    #   setpoint, and print the new current speed.
+    print the new current speed.
     def dec_speed(set_speed)
         @current_speed -= set_speed.abs
         puts "Decrease the current speed to #{@current_speed} MPH"
     end
 
+    # Method shutdown sets current_speed to zero and puts status.
     def shutdown
         @current_speed = 0
         puts "Parked the car"
     end
 
+    # Method current_speed puts the current speed.
     def current_speed
         puts "Car is currently travelling at #{@current_speed} MPH"
     end
 
+    # Method accepts new_color parameter and updates color attribute, then puts
+    #   the new color.
     def spray_paint(new_color)
         self.color = new_color
         puts "The new color of your car is #{@color}"
     end
 end
 
-
+# Create new class instance and pass initial attributes.
 honda = MyCar.new(2007, "silver", "element")
 
+# Test class methods by calling functions, and checking functionality.
 honda.current_speed
 honda.inc_speed(-20)
 honda.current_speed
