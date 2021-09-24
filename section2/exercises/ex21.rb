18dd91c3388aca7754208de2c812946439118704
Tim Roeder <timothyjroeder@gmail.com>
2021-09-24 10:24:27 -0600

    Refactored ex21.rb, syntax and format changes

--- section2/exercises/ex21.rb
+++ section2/exercises/ex21.rb
@@ -1,18 +1,27 @@
+# Learn Ruby the Hard Way exercise #21.
+
+# Add two variables method. Print description of the addition, return result.
 def add(a, b)
   puts "ADDING #{a} + #{b}"
   return a + b
 end
 
+# Subtract two variables method. Print description of the subtraction, return
+#   result.
 def subtract(a, b)
   puts "SUBTRACTING #{a} - #{b}"
   return a - b
 end
 
+# Multiply two variables method. Print description of the multiplication, return
+#   result.
 def multiply(a, b)
   puts "MULTIPLYING #{a} * #{b}"
   return a * b
 end
 
+# Divide two variables method. Print description of the division, return
+#   result.
 def divide(a, b)
   puts "DIVIDING #{a} / #{b}"
   return a / b
@@ -21,13 +30,14 @@ end
 
 puts "Let's do some math with just functions!"
 
+# Save the result of the math operations to new variables.
 age = add(30, 5)
 height = subtract(78, 4)
 weight = multiply(90, 2)
 iq = divide(100, 2)
 
+# Print the results of the new variables. 
 puts "Age: #{age}, Height: #{height}, Weight: #{weight}, IQ: #{iq}"
-
 
 # A puzzle for the extra credit, type it in anyway.
 puts "Here is a puzzle."
