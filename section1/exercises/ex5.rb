5fe859ed8a7479e7e58d81f0603be4a88953ed2f
Tim Roeder <timothyjroeder@gmail.com>
2021-09-24 02:49:38 -0600

    Refactored ex5.rb, syntax and format changes

--- section1/exercises/ex5.rb
+++ section1/exercises/ex5.rb
@@ -1,19 +1,34 @@
+# Learn Ruby the Hard Way exercise #5.
+
+# Declare variables.
 name = 'Zed A. Shaw'
-age = 35 # not a lie in 2009
+age = 35
 height = 74.0 # inches
-inches2si = 2.54 # 1 inch = 2.54 cm
+# SI converision: 1 inch = 2.54 cm
+height2si = height * 2.54 # cm
 weight = 180.0 # lbs
-lbs2si = 0.454 # 1 lb = 0.454 kg
+# SI conversion: 1 lb = 0.454 kg
+weight2si = weight * 0.454 # kgs
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
 
+# String interpolation outputs.
 puts "Let's talk about #{name}."
-puts "He's #{height * inches2si} inches tall." # Need to update text to reflect change to SI units.
-puts "He's #{weight * lbs2si} pounds heavy." # Need to update text to reflect change to SI units.
+
+
+puts "He's #{height} inches tall."
+
+# Print updated SI height and string.
+puts "He's #{height2si} centimeters tall."
+puts "He's #{weight} pounds heavy." 
+
+# Print updated SI weight and string.
+puts "He's #{weight2si} Kgs heavy." 
 puts "Actually that's not too heavy."
 puts "He's got #{eyes} eyes and #{hair} hair."
 puts "His teeth are usually #{teeth} depending on the coffee."
 
-# this line is tricky, try to get it exactly right
-puts "If I add #{age}, #{height}, and #{weight * lbs2si} I get #{age + height * inches2si + weight * lbs2si}."
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+# Print updated SI weight and height string interpolation.
+puts "If I add #{age}, #{height2si}, and #{weight2si} I get #{age + height2si + weight2si}."
