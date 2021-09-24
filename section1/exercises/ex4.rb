ee6284ef798606b30fccfb981ae8cf895adf1456
Tim Roeder <timothyjroeder@gmail.com>
2021-09-24 02:35:37 -0600

    Refactored ex4.rb, syntax and format changes

--- section1/exercises/ex4.rb
+++ section1/exercises/ex4.rb
@@ -1,3 +1,5 @@
+# Learn Ruby the Hard Way exercise #4.
+
 # Variable declarations.
 cars = 100
 space_in_a_car = 4.0
@@ -9,7 +11,7 @@ carpool_capacity = cars_driven * space_in_a_car
 average_passengers_per_car = passengers / cars_driven
 
 
-# Calculation outputs.
+# String interpolation outputs for declared variables.
 puts "There are #{cars} cars available."
 puts "There are only #{drivers} drivers available."
 puts "There will be #{cars_not_driven} empty cars today."
