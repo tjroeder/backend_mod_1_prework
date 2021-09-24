0192337af7b99017100b9c3dc3af8c7449baf798
Tim Roeder <timothyjroeder@gmail.com>
2021-09-24 02:22:03 -0600

    Refactored ex3.rb, syntax and format changes

--- section1/exercises/ex3.rb
+++ section1/exercises/ex3.rb
@@ -1,32 +1,42 @@
-puts "I will now count my chickens:"
+# Learn Ruby the Hard Way exercise #3.
 
-# This line counts the number of hens in the string insert by dividing 30 by 6, than adding 25.
+puts 'I will now count my chickens:'
+
+# Prints number of hens in the string interpolation by dividing 30 by 6 than
+#   adding 25.
 puts "Hens #{25.0 + 30.0 / 6.0}"
-# This line counts the number of roosters in the string insert by multiplying 25 and 3, then dividing by 4, and taking the remainder and subtracting from 100.
+
+# Prints number of roosters in the string interpolation by multiplying 25 and
+#   3, then dividing by 4, and taking the remainder and subtracting from 100.
 puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
 
-puts "Now I will count the eggs:"
+puts 'Now I will count the eggs:'
 
-# This line counts eggs by dividing 4 by 2, and dividing 1 by 4. Than, adding 3, 2, 1 together, subtracting 5, adding 0, subtracting 0, and adding 6.
+# Prints egg count by dividing 4 by 2, and dividing 1 by 4. Than, adding 3, 2,
+#   1 together, subtracting 5, adding 0, subtracting 0, and adding 6.
 puts (3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
 
-puts "Is it true that 3 + 2 < 5 - 7?"
+puts 'Is it true that 3 + 2 < 5 - 7?'
 
-# This line adds 3 and 2, and subtracts 7 from 5. Than compares both results, whether the first calculation is less than the second.
+# Prints addition of 3 and 2, and subtracts 7 from 5. Than compares both
+#   results, whether the first calculation is less than the second.
 puts 3.0 + 2.0 < 5.0 - 7.0
 
-# This line adds 3 and 2 together.
+# Prints additon of 3 and 2 together.
 puts "What is 3 + 2? #{3.0 + 2.0}"
-# This line subtracts 7 from 5.
+
+# Prints subtraction of 7 from 5.
 puts "What is 5 - 7? #{5.0 - 7.0}"
 
 puts "Oh, that's why it's false."
 
-puts "How about some more."
+puts 'How about some more.'
 
-# This line asks if 5 is greater than -2.
+# Prints result of 5 is greater than -2.
 puts "Is it greater? #{5 > -2}"
-# This line asks if 5 is greater than or equal to -2.
+
+# Prints result of 5 greater than or equal to -2.
 puts "Is is greater or equal? #{5 >= -2}"
-# This line asks if 5 is less than or equal to -2.
+
+# Prints result of  5 less than or equal to -2.
 puts "Is it less or equal? #{5 <= -2}"
