72d0c2188b808b6cad6f298ff7c24e1661af4142
Tim Roeder <timothyjroeder@gmail.com>
2021-09-24 03:12:21 -0600

    Refactored loops.rb, syntax and format changes

--- section1/exercises/loops.rb
+++ section1/exercises/loops.rb
@@ -5,7 +5,7 @@
 
 # Example: Write code that prints your name five times:
 5.times do
-  p "Hermione Granger"
+  p 'Hermione Granger'
 end
 
 # Write code that prints the sum of 2 plus 2 seven times:
@@ -14,9 +14,9 @@ end
 end
 
 # Write code that prints the phrase 'She sells seashells down by the seashore'
-# ten times:
+#   ten times:
 10.times do
-  p "She sells seashells down by the seashore"
+  p 'She sells seashells down by the seashore'
 end
 
 # Write code that prints the result of 5 + 7 a total of 9 timees
