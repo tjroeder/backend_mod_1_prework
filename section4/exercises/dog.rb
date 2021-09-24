bfef07a57e97fd51336d383a94676def3997fc81
Tim Roeder <timothyjroeder@gmail.com>
2021-09-24 16:33:30 -0600

    Refactored dog.rb, added annotations

--- section4/exercises/dog.rb
+++ section4/exercises/dog.rb
@@ -2,9 +2,12 @@
 # the dog hungry. Call that method below the class, and 
 # print the dog's hunger status.
 
+# Declare Dog class.
 class Dog
+  # Write protected attributes.
   attr_reader :breed, :name, :age, :hungry
 
+  # Initialize the Dog attributes.
   def initialize(breed, name, age)
     @breed  = breed
     @name   = name
@@ -12,28 +15,39 @@ class Dog
     @hungry = true
   end
 
+  # Method to print string to console.
   def bark
     p "woof!"
   end
 
+  # Method to change hungry status to false.
   def eat
     @hungry = false
   end
 
+  # Puts string interpolation to the console. Change hungry status to true.
   def play
     puts "#{name} got zoomies playing fetch!"
     @hungry = true
   end
-
 end
 
+# Create a new class instance, and initalize variables.
 fido = Dog.new("Bernese", "Fido", 4)
+
+# Test printing the instance variables.
 p fido.breed
 p fido.name
 p fido.age
 p fido.hungry
+
+# Test changing hungry status to false by calling eat method.
 fido.eat
+# Print results of hungry change.
 p fido.hungry
 
+# Test changing hungry status to true by calling play method.
 fido.play
+# Print results of the hungry change with string interpolation. Output will be
+#   different dependent on hungry status.
 p "#{fido.name} is #{fido.hungry == true ? 'hungry': 'not hungry'}"
