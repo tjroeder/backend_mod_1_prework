253c62bf9e9898485189cf5fb227ddb7530980b7
Tim Roeder <timothyjroeder@gmail.com>
2021-09-24 16:00:33 -0600

    Refactored say.rb, added annotations to method

--- section2/exercises/say.rb
+++ section2/exercises/say.rb
@@ -3,11 +3,13 @@
 # puts "how are you"
 # puts "I'm fine"
 
-
+# Method prints the input to the user with string interpolation. If no
+#   arguement is given, return default "hello" statement.
 def say(words = "hello")
   puts words + '.'
 end
 
+# Testing say method with multiple inputs.
 say()
 say("hello")
 say("hi")
