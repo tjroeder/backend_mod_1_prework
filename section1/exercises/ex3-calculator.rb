565b0598d794caeaea4835c142891eb6cc6cbb19
Tim Roeder <timothyjroeder@gmail.com>
2021-09-24 02:33:26 -0600

    Refactored ex3-calculator.rb, syntax and format changes

--- section1/exercises/ex3-calculator.rb
+++ section1/exercises/ex3-calculator.rb
@@ -1,17 +1,20 @@
+# Learn Ruby the Hard Way exercise #3 Calulator.
+
 # This is a custom calculation... by you!
 
-puts "Hey you! What two integers do you want to add together?"
+puts 'Hey you! What two integers do you want to add together?'
 
-# This requests two integers from the user.
+# Request two integers from the user and store the values.
 number1 = gets.chomp
 number2 = gets.chomp
 
-# This takes the two number inputs and converts to an int. If the number cannot convert to an int it sets the test variables to true.
+# Check the user inputs can convert to integers. Save the new integer values.
 test1 = Integer(number1) rescue false
 test2 = Integer(number2) rescue false
 
-# Checks if test1 or test2 are true. If they are a non-int was entered in the prompt.
-if (!(test1) || !(test2)) == true
+# Checks if user inputs are integers. If they are a non-int was entered in the
+#   prompt.
+if (!test1 || !test2) == true
   puts "I said an integer! You fail."
   return
 end
