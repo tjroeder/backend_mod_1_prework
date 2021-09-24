0dd0c204c065b658b3f8283c429130d92ee5d0d6
Tim Roeder <timothyjroeder@gmail.com>
2021-09-24 03:00:14 -0600

    Refactored ex11.rb, syntax and format changes

--- section1/exercises/ex11.rb
+++ section1/exercises/ex11.rb
@@ -1,3 +1,6 @@
+# Learn Ruby the Hard Way exercise #11.
+
+# Print prompts to request user input. Save user inputs to variables.
 print "How old are you? "
 age = gets.chomp
 print "How tall are you? "
@@ -5,9 +8,10 @@ height = gets.chomp
 print "How much do you weigh? "
 weight = gets.chomp
 
+# Print concatenated string.
 puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
 
-
+# Print prompts to request user input. Save user inputs to variables.
 print "What is your name? "
 name = gets.chomp
 print "What is your quest? "
@@ -15,4 +19,5 @@ quest = gets.chomp
 print "What is the capital of Assyria? "
 capital = gets.chomp
 
+# Print concatenated string.
 puts "Your name is #{name}, and your quest is #{quest}. And you thought the capital of Assyria was #{capital}?! Into the chasm with you!"
