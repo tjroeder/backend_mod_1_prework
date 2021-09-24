70e2d0e17a00b12aa579df16e4e614476ce87465
Tim Roeder <timothyjroeder@gmail.com>
2021-09-24 03:10:13 -0600

    Refactored interpolation.rb, syntax and format changes

--- section1/exercises/interpolation.rb
+++ section1/exercises/interpolation.rb
@@ -4,30 +4,32 @@
 # `ruby section1/exercises/interpolation.rb`
 
 # Example: Write code that uses the variables below to form a string that reads
-# "The Chudley Cannons are Ron's favorite Quidditch team":
+#   "The Chudley Cannons are Ron's favorite Quidditch team":
 name = "Ron"
 team = "Chudley Cannons"
 
 p "The #{team} are #{name}'s favorite Quidditch team"
 
 # Write code that uses the variables below to form a string that reads
-# "The quick red fox jumped over the lazy brown dog":
-speedy = "quick red fox"
-slow_poke = "lazy brown dog"
+#   "The quick red fox jumped over the lazy brown dog":
+speedy = 'quick red fox'
+slow_poke = 'lazy brown dog'
 
 p "The #{speedy} jumped over the #{slow_poke}"
 
 # Write code that uses the variables below to form a string that reads
-# "In a predictable result, the tortoise beat the hare!":
-slow_poke = "tortoise"
-speedy = "hare"
+#   "In a predictable result, the tortoise beat the hare!":
+slow_poke = 'tortoise'
+speedy = 'hare'
 
 p "In a predictable result, the #{slow_poke} beat the #{speedy}!"
 
 
 # YOU DO:
-# Declare three variables, name/content/data type of your choice. Think carefully about what you name the variables.
-# Remember, the goal is to be concise but descriptive (it's a hard balance!) Then, log out ONE sentence that incorporates all THREE variables.
+# Declare three variables, name/content/data type of your choice. Think
+#   carefully about what you name the variables.
+# Remember, the goal is to be concise but descriptive (it's a hard balance!)
+#   Then, log out ONE sentence that incorporates all THREE variables.
 food = "tacos"
 flavor = "carnitas"
 condiment = "spicy avocado sauce"
